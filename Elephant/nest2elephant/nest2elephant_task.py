f9e5daae76597582f22741a6dbe994535faafff6
Jakob Jordan <j.jordan@fz-juelich.de>
2015-09-11 13:34:57 +0200

    now possible to define list of ids to extract from gdf file in nest2elephant

--- Elephant/nest2elephant/nest2elephant_task.py
+++ Elephant/nest2elephant/nest2elephant_task.py
@@ -5,7 +5,7 @@ from active_worker.task import task
 
 
 @task
-def nest2elephant_task(input_filename, t_start, t_stop):
+def nest2elephant_task(input_filename, t_start, t_stop, gdf_id_list):
     '''
         Task Manifest Version: 1
         Full Name: nest2elephant_task
@@ -20,19 +20,25 @@ def nest2elephant_task(input_filename, t_start, t_stop):
         Accepts:
             input_file:
                 type: application/vnd.juelich.nest.spike_times
-                description: Input file that contains spiking data from a NEST simulation in gdf format.
+                description: Input file that contains spiking data from a
+                NEST simulation in gdf format.
             t_start:
                 type: double
                 description: Start time of spike train recording.
             t_stop:
                 type: double
                 description: Stop time of spike train recording.
+            gdf_id_list:
+                type: list(long)
+                description: Neuron IDs in the input file that should be
+                extracted. Provide an empty list to extract all neurons
+                with at least one spike.
         Returns:
             res: application/unknown
     '''
 
     input_file = neo.io.GdfIO(input_filename)
-    seg = input_file.read_segment(gdf_id_list=[], t_start=t_start*pq.ms, t_stop=t_stop*pq.ms)
+    seg = input_file.read_segment(gdf_id_list=gdf_id_list, t_start=t_start*pq.ms, t_stop=t_stop*pq.ms)
     output_filename = input_filename.split('.')[0] + '.h5'
     output_file = neo.io.NeoHdf5IO(output_filename)
     output_file.write(seg.spiketrains)
@@ -44,4 +50,5 @@ if __name__ == '__main__':
     input_filename = 'collected_spikes_L6I-296.gdf'
     t_start = 0.
     t_stop = 300.
-    nest2elephant_task(input_filename, t_start, t_stop)
+    gdf_id_list = []
+    nest2elephant_task(input_filename, t_start, t_stop, gdf_id_list)
