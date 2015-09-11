ebd280c7d12c1de73f5d81c3e17fdddcf1e9f80d
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2015-09-11 15:27:12 +0200

    added bundle mode to histogram task to output multiple files

--- Elephant/cv_histogram_task/cv_histogram_task.py
+++ Elephant/cv_histogram_task/cv_histogram_task.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python
-__author__ = "Long Phan. INM-6, FZJ"
+__author__ = "Long Phan, Alper Yegenoglu. INM-6, FZJ"
 
 import numpy as np
 import elephant as el
 import matplotlib
+import json
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from neo import io
@@ -13,7 +14,7 @@ from task_types import TaskTypes as tt
 
 @task
 def cv_histogram_task(input_data):
-    '''
+    """
         Task Manifest Version: 1
         Full Name: cv_histogram_task
         Caption: cv_histogram
@@ -28,8 +29,8 @@ def cv_histogram_task(input_data):
             input_data: application/unknown
 
         Returns:
-            res: image/png
-    '''
+            res: application/vnd.juelich.bundle.elephant.data
+    """
 
     # stage the input file
     original_path = cv_histogram_task.task.uri.get_file(input_data)
@@ -37,6 +38,9 @@ def cv_histogram_task(input_data):
     # open file from path using Neo
     ion = io.NeoHdf5IO(filename=original_path)
     number_of_SpikeTrains = ion.get_info()['SpikeTrain']
+
+    my_bundle_mimetype = "application/vnd.juelich.bundle.elephant.data"
+    bundle = cv_histogram_task.task.uri.build_bundle(my_bundle_mimetype)
 
     # Init result
     res = []
@@ -61,7 +65,7 @@ def cv_histogram_task(input_data):
     # Close remaining file h5
     ion.close()
 
-    print res
+    # print res
 
     # Create plot
     plt.title("CV Histogram")
@@ -75,12 +79,28 @@ def cv_histogram_task(input_data):
     else:
         print 'Warning: could not calculate CV for input(s).'
 
-    output_file = 'result_cv_histogram_task.png'
-    with open(output_file, 'w') as output:
+    output_file_img = 'result_cv_histogram_task.png'
+    output_file = output_file_img.split('.')[0]+str('.json')
+    with open(output_file_img, 'w') as output:
         plt.savefig(output)
-    return cv_histogram_task.task.uri.save_file(mime_type='image/png',
-                                                src_path=output_file,
-                                                dst_path=output_file)
+
+    # Add image meta data to bundle
+    bundle.add_file(mime_type='image/png',
+                    src_path=output_file_img,
+                    bundle_path=output_file_img,
+                    dst_path=output_file_img)
+
+    # Write a file containing the results
+    with open(output_file, 'w') as f:
+        json.dump(res, f)
+    f.close()
+
+    # Add json meta data to bundle
+    bundle.add_file(mime_type='application/unknown',
+                    src_path=output_file,
+                    bundle_path=output_file,
+                    dst_path=output_file)
+    return bundle.save('cv_histogram_task_bundle')
 
 
 if __name__ == '__main__':
