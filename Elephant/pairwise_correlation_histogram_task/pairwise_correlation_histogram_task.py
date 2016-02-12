3f92942eed07286b00f753a1c8e0a06675f3b96c
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2016-02-12 17:29:59 +0100

    fixed errors in pairwise correlation

--- Elephant/pairwise_correlation_histogram_task/pairwise_correlation_histogram_task.py
+++ Elephant/pairwise_correlation_histogram_task/pairwise_correlation_histogram_task.py
@@ -6,8 +6,13 @@
 
 from active_worker.task import task
 from task_types import TaskTypes as tt
-import numpy as np
-import h5py
+from elephant.conversion import BinnedSpikeTrain
+from elephant.spike_train_correlation import corrcoef
+import quantities as pq
+import neo
+import matplotlib.pyplot as plt
+import os
+
 
 @task
 def pairwise_correlation_task(inputdata, binsize):
@@ -24,20 +29,16 @@ def pairwise_correlation_task(inputdata, binsize):
         Compatible_queues: ['cscs_viz']
         Accepts:
             inputdata: application/unknown
-            binsize: int    
+            binsize: int
+
         Returns:
             res: application/unknown
     '''
-    import quantities as pq
-    import neo
-    import elephant
-
     # =========================================================================
     # Load data
     # =========================================================================
-
     # stage the input file
-    original_path = pairwise_correlation_histogram_task.task.uri.get_file(inputdata)
+    original_path = pairwise_correlation_task.task.uri.get_file(inputdata)
 
     session = neo.NeoHdf5IO(filename=original_path)
     block = session.read_block()
@@ -48,11 +49,8 @@ def pairwise_correlation_task(inputdata, binsize):
     # =========================================================================
     # Pairwise_correlation_histogram
     # =========================================================================
-
-    from elephant.conversion import BinnedSpikeTrain
-    from elephant.spike_train_correlation import corrcoef
-    cc_matrix = corrcoef(BinnedSpikeTrain(sts, binsize=binsize*ms))
-    outputname = 'pairwise_correlations_result.h5'
+    cc_matrix = corrcoef(BinnedSpikeTrain(sts, binsize=binsize * pq.ms))
+    output_filename = 'pairwise_correlations_result.h5'
 
     plt.hist(cc_matrix, 100)
     plt.xlabel('$\\rho$')
@@ -62,10 +60,11 @@ def pairwise_correlation_task(inputdata, binsize):
     with open(output_filename, 'w') as result_pth:
         plt.savefig(result_pth, dpi=100)
     dst_name = os.path.basename(output_filename)
-    return pairwise_correlation_histogram_task.task.uri.save_file(mime_type='image/png',
-                                         src_path=output_filename,
-                                         dst_path=dst_name)
+    return pairwise_correlation_task.task.uri.save_file(mime_type='image/png',
+                                                        src_path=output_filename,
+                                                        dst_path=dst_name)
 
 if __name__ == '__main__':
-    inputdata = tt.URI('application/unknown', '../../experiment.h5')
-    pairwise_correlation_histogram_task(inputdata, binsize = 5)
+    inputdata = tt.URI('application/unknown', 'experiment.h5')
+    binsize = 5
+    pairwise_correlation_task(inputdata, binsize)
