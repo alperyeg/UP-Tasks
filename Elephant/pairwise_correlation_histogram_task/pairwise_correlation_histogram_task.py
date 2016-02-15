2e44265333dc1221a225e211eafe773759ef0458
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2016-02-15 19:12:30 +0100

    changed to bundle strucutre

--- Elephant/pairwise_correlation_histogram_task/pairwise_correlation_histogram_task.py
+++ Elephant/pairwise_correlation_histogram_task/pairwise_correlation_histogram_task.py
@@ -12,6 +12,7 @@ import quantities as pq
 import neo
 import matplotlib.pyplot as plt
 import os
+import h5py
 
 
 @task
@@ -24,13 +25,20 @@ def pairwise_correlation_histogram_task(inputdata, binsize):
         Description: |
             This task calculates all pair-wise correlations between all
             combinations of spike trains in the input file.
+            The output is a bundle containing an png file showing the 
+            correlation result as well as an h5 file with the cross-correlation
+            results in a numpy 2d-array.
         Categories:
             - FDAT
         Compatible_queues: ['cscs_viz']
         Accepts:
-            inputdata: application/unknown
-            binsize: int
-
+            inputdata:
+                type: application/unknown
+                description: Input file that contains spiking data from a
+                    HDF5 file.
+            binsize:
+                type: int
+                description: Bin width to bin the spike trains. Uses ms resolution.
         Returns:
             res: application/unknown
     '''
@@ -39,6 +47,7 @@ def pairwise_correlation_histogram_task(inputdata, binsize):
     # =========================================================================
     # stage the input file
     original_path = pairwise_correlation_histogram_task.task.uri.get_file(inputdata)
+    bundle = pairwise_correlation_histogram_task.task.uri.build_bundle("application/unkown")
 
     session = neo.NeoHdf5IO(filename=original_path)
     block = session.read_block()
@@ -50,20 +59,34 @@ def pairwise_correlation_histogram_task(inputdata, binsize):
     # Pairwise_correlation_histogram
     # =========================================================================
     cc_matrix = corrcoef(BinnedSpikeTrain(sts, binsize=binsize * pq.ms))
-    output_filename = 'pairwise_correlations_result.h5'
+    filename = 'pairwise_correlations_result'
 
+    # Store to hdf5
+    f = h5py.File(os.path.splitext(filename)[0] + '.h5', 'w')
+    dataset = f.create_dataset("cc_matrix", data=cc_matrix)
+    f.close()
+
+    output_filename = os.path.splitext(filename)[0] + '.png'
+
+    # Plotting
     plt.hist(cc_matrix, 100)
     plt.xlabel('$\\rho$')
     plt.ylabel('PDF')
-
-    output_filename = os.path.splitext(original_path)[0] + '.png'
     with open(output_filename, 'w') as result_pth:
         plt.savefig(result_pth, dpi=100)
-    dst_name = os.path.basename(output_filename)
+
+    dst_name = os.path.basename(filename)
     session.close()
-    return pairwise_correlation_histogram_task.task.uri.save_file(mime_type='image/png',
-                                                                  src_path=output_filename,
-                                                                  dst_path=dst_name)
+
+    bundle.add_file(src_path=filename,
+                    dst_path=dst_name,
+                    bundle_path=dst_name,
+                    mime_type="image/png")
+    bundle.add_file(src_path=filename,
+                    dst_path=dst_name,
+                    bundle_path=dst_name,
+                    mime_type="application/unkown")
+    return bundle.save("bundle_" + filename)
 
 if __name__ == '__main__':
     inputdata = tt.URI('application/unknown', 'experiment.h5')
