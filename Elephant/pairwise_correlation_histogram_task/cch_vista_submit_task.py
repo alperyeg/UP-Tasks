26782bd557be5520833525115bc8a48ac834b6a0
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2016-03-03 10:29:04 +0100

    updated task script and added h5 wrapper

--- Elephant/pairwise_correlation_histogram_task/cch_vista_submit_task.py
+++ Elephant/pairwise_correlation_histogram_task/cch_vista_submit_task.py
@@ -19,7 +19,7 @@ def load_local_file(name):
 
 @task
 def cch_vista_submit_task(inputdata_spinnaker, inputdata_nest, run_script,
-                          aux_script):
+                          collect_script):
     '''
         Task Manifest Version: 1
         Full Name: cch_vista_submit_task
@@ -41,34 +41,54 @@ def cch_vista_submit_task(inputdata_spinnaker, inputdata_nest, run_script,
                 type: application/unknown
                 description: Input file that contains spiking data from a
                     HDF5 file from nest simulation.
-            script:
+            run_script:
+                type: application/unkown
+                description: Script which will be executed on an HPC.
+            collect_script:
                 type: application/unkown
                 description: Script which will be executed on an HPC.
 
         Returns:
             res: application/unknown
     '''
-    # TODO add the h5py wrapper script
-    spinnaker_data_path = cch_vista_submit_task.task.uri.get_file(inputdata_spinnaker)
+    # Get paths
+    spinnaker_data_path = cch_vista_submit_task.task.uri.get_file(
+        inputdata_spinnaker)
     nest_data_path = cch_vista_submit_task.task.uri.get_file(inputdata_nest)
     submit_script_path = cch_vista_submit_task.task.uri.get_file(run_script)
-    collect_script_path = cch_vista_submit_task.task.uri.get_file(aux_script)
-    code = {'To': '{}'.format(os.path.split(submit_script_path)[1]), 'Data': load_local_file('{}'.format(submit_script_path))}
-    collect_script = {'To': '{}'.format(os.path.split(collect_script_path))[1], 'Data': load_local_file('{}'.format(collect_script_path))}
-    spinnaker_data = {'To': '{}'.format(os.path.split(spinnaker_data_path))[1], 'Data': load_local_file('{}'.format(spinnaker_data_path))}
-    nest_data = {'To': '{}'.format(os.path.split(nest_data_path))[1], 'Data': load_local_file('{}'.format(nest_data_path))}
-    inputs = [code, collect_script, spinnaker_data, nest_data]
+    collect_script_path = cch_vista_submit_task.task.uri.get_file(collect_script)
+    # Load h5 wrapper
+    h5_wrapper = 'wrapper.py'
+    wrapper_path = os.path.join(os.path.dirname(__file__), h5_wrapper)
 
+    # Preparing for unicore submission
+    code = {'To': '{}'.format(os.path.split(submit_script_path)[1]),
+            'Data': load_local_file('{}'.format(submit_script_path))}
+    collect_script = {'To': '{}'.format(os.path.split(collect_script_path)[1]),
+                      'Data': load_local_file('{}'.format(collect_script_path)[1])}
+    h5_script = {'To': '{}'.format(os.path.split(wrapper_path)[1]),
+                 'Data': load_local_file('{}'.format(wrapper_path)[1])}
+    spinnaker_data = {'To': '{}'.format(os.path.split(spinnaker_data_path)[1]),
+                      'Data': load_local_file('{}'.format(spinnaker_data_path))[1]}
+    nest_data = {'To': '{}'.format(os.path.split(nest_data_path)[1]),
+                 'Data': load_local_file('{}'.format(nest_data_path))}
+    inputs = [code, collect_script, h5_script, spinnaker_data, nest_data]
+
+    # Get token
     oauth_token = cch_vista_submit_task.task.uri.get_oauth_token()
     auth = unicore_client.get_oidc_auth(oauth_token)
-    job = {}
+
+    # Unicore parameter
+    job = dict()
     job['ApplicationName'] = 'Elephant'
     job['Parameters'] = {'INPUT': submit_script_path}
     job['Environment'] = {'spinnaker_data': os.path.split(spinnaker_data_path)[1],
                           'nest_data': os.path.split(nest_data_path)[1]}
-    job['User postcommand'] = 'python {0} {1}'.format(os.path.split(collect_script_path)[1], spinnaker_data)
+    job['User postcommand'] = 'python {0} {1}'.format(
+        os.path.split(collect_script_path)[1], spinnaker_data)
     job['RunUserPostCommandOnLoginNode'] = 'false'
 
+    # Submission
     base_url = unicore_client.get_sites()['JURECA']['url']
     job_url = unicore_client.submit(os.path.join(base_url, 'jobs'), job, auth,
                                     inputs)
