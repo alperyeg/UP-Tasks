6a60ae7a328147561f46972a18af71f143c1adc5
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2016-03-14 11:39:55 +0100

    changed bundle to single file

--- Elephant/pairwise_correlation_histogram_task/cch_vista_submit_task.py
+++ Elephant/pairwise_correlation_histogram_task/cch_vista_submit_task.py
@@ -52,7 +52,7 @@ def cch_vista_submit_task(inputdata_spinnaker, inputdata_nest, run_script,
                 description: Number of tasks which will be run on the HPC.
 
         Returns:
-            res: application/vnd.juelich.bundle.elephant.data
+            res: application/unknown
     '''
     # Get paths
     spinnaker_data_path = cch_vista_submit_task.task.uri.get_file(
@@ -95,42 +95,43 @@ def cch_vista_submit_task(inputdata_spinnaker, inputdata_nest, run_script,
                                     'PostCommands': ['COLLECT']}
 
     # (hackish) export to dCache for visualisation
-    results = ['viz_output_nest.h5', 'viz_output_nest.pkl', 
-            'viz_output_spinnaker.h5', 'viz_output_spinnaker.pkl']
+    results = ['viz_output_nest.h5', 'viz_output_nest.pkl',
+               'viz_output_spinnaker.h5', 'viz_output_spinnaker.pkl']
     exports = []
     for result in results:
-        exports.append({'From' : 'results/'+result,
-        'To' : 'https://jade01.zam.kfa-juelich.de:2880/HBP/summit15/nest-elephant/'+result,
-         'Credentials': {'Username': 'jbiddiscombe', 'Password': 'Aithahs8'},
-         'FailOnError': 'false',
-        })
+        exports.append({'From': 'results/' + result,
+                        'To': 'https://jade01.zam.kfa-juelich.de:2880/HBP/summit15/nest-elephant/' + result,
+                        'Credentials': {'Username': 'jbiddiscombe',
+                                        'Password': 'Aithahs8'},
+                        'FailOnError': 'false',
+                        })
     job['Exports'] = exports
-    
+
     # Submission
     base_url = unicore_client.get_sites()['JURECA']['url']
     job_url = unicore_client.submit(os.path.join(base_url, 'jobs'), job, auth,
                                     inputs)
     print "Submitting to {}".format(job_url)
-    unicore_client.wait_for_completion(job_url, auth, refresh_function = cch_vista_submit_task.task.uri.get_oauth_token)
+    unicore_client.wait_for_completion(job_url, auth,
+                                       refresh_function=cch_vista_submit_task.task.uri.get_oauth_token)
 
     # Get results and store them to task-local storage
     # create bundle & export bundle
-    my_bundle_mimetype = "application/vnd.juelich.bundle.elephant.data"
-    bundle = cch_vista_submit_task.task.uri.build_bundle(my_bundle_mimetype)
-
     workdir = unicore_client.get_working_directory(job_url, auth)
+    fn = ''
     for filename in results:
-        content = unicore_client.get_file_content(workdir+"/files/results/"+filename,auth)
-        with open(result,"w") as local_file:
-              local_file.write(content)
-        bundle.add_file(src_path=filename,
-                        dst_path=os.path.join('contents', filename),
-                        bundle_path=filename,
-                        mime_type="application/unknown")
+        content = unicore_client.get_file_content(
+            workdir + "/files/results/" + filename, auth)
+        with open(filename, "w") as local_file:
+            local_file.write(content)
+            fn = filename
 
-    my_bundle_name = 'elephant_bundle'
-    return bundle.save(my_bundle_name)
-            
+    return cch_vista_submit_task.task.uri.save_file(mime_type='application/unknown',
+                                                    src_path=fn,
+                                                    dst_path=os.path.join(
+                                                        'contents', fn))
+
+
 if __name__ == '__main__':
     inputdata_spinnaker = tt.URI('application/unknown', 'spikes_L5E.h5')
     inputdata_nest = tt.URI('application/unknown', 'spikes_L5E.h5')
